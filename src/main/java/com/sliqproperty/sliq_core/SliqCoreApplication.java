05e9cd428932bb54628fdf6ea9951a67cb4d2317
phuluso <phulusojay@gmail.com>
2024-11-11 18:57:10 +0200

    init

---
+++ src/main/java/com/sliqproperty/sliq_core/SliqCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sliqproperty.sliq_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliqCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SliqCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliqproperty/sliq_core/SliqCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliqproperty.sliq_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliqCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
